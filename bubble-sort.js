d3915210e8b10a1a2a6a9c8b06df9b4805400fff
Elar Saks <elarsaks@gmail.com>
2022-08-21 13:06:30 +0300

    Add comments

--- bubble-sort.js
+++ bubble-sort.js
@@ -2,27 +2,37 @@ const test1 = [8, 5, 2, 9, 5, 6, 3]
 
 
 function bubbleSort(array) {
+  // Insert value into hadToSort to start do while loop
   let hadToSort = [true]
 
   do {
+    // Set hadToSort to empty (false) on every iteration
     hadToSort = []
-    
+
+    // Loop over an array
     for (let i = 0; i < array.length; i++){
+
+        // Assign left and right pointer
         let leftPointer = array[i-1]
         let rightPointer = array[i]
 
-     
+       // If right pointer value is bigger than left pointer value
         if(leftPointer > rightPointer){
+          // Swap values in array
           array[i-1] = rightPointer
           array[i] = leftPointer
+
+          // Set has to sort true
           hadToSort.push(true)
         }
-    }    
+    }
+
+    // Call this for loop until hadToSort return false
   } while (hadToSort.length > 0)
 
+    // At the end return sorted array
     return array
 }
-
 // Do not edit the line below.
 //exports.bubbleSort = bubbleSort;
 
