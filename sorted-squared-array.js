17b96a0baf3ce53a1dae9fcec69b479ce3eb2e19
Elar Saks <elarsaks@gmail.com>
2022-09-05 18:46:26 +0300

    Fix function and add comments

--- sorted-squared-array.js
+++ sorted-squared-array.js
@@ -1,9 +1,10 @@
 function sortedSquaredArray(array) {
-  let resp = Array(array.length)
-  let start = 0
-  let end = array.length-1
+  let resp = Array(array.length - 1)
+  let start = 0 // Point at the start
+  let end = array.length-1 // Point at the end
 
-  for(let i=array.length-1; i!=0; i--){
+  // Loop over array
+  for(let i=array.length-1; i >= 0; i--){
     // If end is bigger than start
     if(Math.abs(array[start]) < Math.abs(array[end])){
       resp[i] = array[end] * array[end]
