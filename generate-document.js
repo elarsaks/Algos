9b255d5cff27bf5cf1b7b6ddd8743ae8905a258c
Elar Saks <elarsaks@gmail.com>
2022-09-10 21:19:08 +0300

    Fix generate document.

--- generate-document.js
+++ generate-document.js
@@ -1,29 +1,40 @@
 function generateDocument(characters, document) {
-  // Return instantly false if strings are not the same size
-  if(characters.length != document.length) return false
+  // Return FALSE if there is not enough characters 
+  if(characters.length > document.lenth) return false
 
-  // Create character &document hashMap 
+  // Create character & document hashMap 
   let charMap = {}
   let docMap = {}
 
   // Loop over strings
   for(let i=0; i<characters.length; i++){
+    
     // Map out characters
-    charMap = charMap[characters[i]] ? charMap[characters[i]] = characters[i] + 1
-      : charMap[characters[i]] = 1
+    charMap[characters[i]] 
+        ? charMap[characters[i]] += 1 
+        : charMap[characters[i]] = 1
 
-    // Map out document
-    docMap = docMap[document[i]] 
-      ? docMap[document[i]] = document[i] + 1
-      : docMap[document[i]] = 1
+    // Map out document (stop mapping if document has ended, because characters is longer)
+    if(document[i]){ 
+        docMap[document[i]] 
+        ? docMap[document[i]] += 1
+        : docMap[document[i]] = 1
+    }
+
   }
 
-  // TODO: compare charmap to document Map
-  for(const [key, value] of Object.entries(docMap)){
-    if(charMap[key] != value){
-      return false
-    }
+  // Compare charmap to document Map
+  for(const [docMapKey, docMapValue] of Object.entries(docMap)){
+    // Return FALSE if characters dont have requested char
+    if(docMapKey && !charMap[docMapKey]) return false
+    // Return FALSE if characters dont have enough requested chars
+    if(charMap[docMapKey] < docMapValue) return false 
   }
 
   return true;
 }
+
+const characters = 'Bste!hetsi ogEAxpelrt x '
+const document = 'AlgoExpert is the Best!'
+
+console.log(generateDocument(characters, document))
