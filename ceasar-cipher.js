525ffcd8688f64e09f268192d9ac7f5263b5424f
Elar Saks <elarsaks@gmail.com>
2022-09-10 13:06:45 +0300

    Add second solution

--- ceasar-cipher.js
+++ ceasar-cipher.js
@@ -12,4 +12,28 @@ function caesarCipherEncryptor(string, key) {
   return newString
 }
 
+
 console.log(caesarCipherEncryptor('abc', 3))
+
+/*
+function caesarCipherEncryptor(string, key) {
+
+  const alphabet =[ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+  'm', 'n', 'o', 'p', 'q', 'r',  's', 't', 'u', 'v', 'w', 'x',
+  'y', 'z' ]
+
+  let newString = ''
+
+  for(let i=0; i<string.length; i++){
+    let stringIdx  = alphabet.indexOf(string.charAt(i))
+    let cipherIdx = stringIdx + key
+
+    if(cipherIdx > alphabet.length - 1){
+        cipherIdx = cipherIdx % alphabet.length
+    }
+
+    newString =  newString + alphabet[cipherIdx]
+  }
+
+  return newString
+} */
